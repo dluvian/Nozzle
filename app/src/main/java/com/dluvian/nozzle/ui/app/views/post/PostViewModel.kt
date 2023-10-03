75cc0a763a22e8b0742d815aec06d430aff2dd0a
dluvian <dluvian@proton.me>
2023-10-03 20:16:56 +0800

    Show shortened npub when quoting unknown author

--- app/src/main/java/com/dluvian/nozzle/ui/app/views/post/PostViewModel.kt
+++ app/src/main/java/com/dluvian/nozzle/ui/app/views/post/PostViewModel.kt
@@ -1,7 +1,6 @@
 package com.dluvian.nozzle.ui.app.views.post
 
 import android.content.Context
-import android.util.Log
 import android.widget.Toast
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
@@ -10,6 +9,7 @@ import com.dluvian.nozzle.R
 import com.dluvian.nozzle.data.nostr.INostrService
 import com.dluvian.nozzle.data.nostr.utils.EncodingUtils.createNeventUri
 import com.dluvian.nozzle.data.nostr.utils.EncodingUtils.nostrStrToNostrId
+import com.dluvian.nozzle.data.nostr.utils.ShortenedNameUtils.getShortenedNpubFromPubkey
 import com.dluvian.nozzle.data.provider.IPersonalProfileProvider
 import com.dluvian.nozzle.data.provider.IRelayProvider
 import com.dluvian.nozzle.data.room.dao.HashtagDao
@@ -29,8 +29,6 @@ import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import java.util.concurrent.atomic.AtomicBoolean
-
-private const val TAG = "PostViewModel"
 
 data class PostViewModelState(
     val content: String = "",
@@ -60,33 +58,25 @@ class PostViewModel(
             viewModelState.value
         )
 
-    init {
-        Log.i(TAG, "Initialize PostViewModel")
-    }
-
     val onPreparePost: () -> Unit = {
         preparePost(postToQuote = null)
     }
 
-    // TODO: Add recipients read relays to selection
     private val isPreparing = AtomicBoolean(false)
-    val onPrepareQuote: (String) -> Unit = { postIdToQuote ->
-        if (!isPreparing.get() && uiState.value.postToQuote?.id != postIdToQuote) {
-            nostrStrToNostrId(postIdToQuote)?.let { nostrId ->
-                Log.i(TAG, "Prepare quoting $postIdToQuote")
-                isPreparing.set(true)
-                viewModelState.update { it.copy(postToQuote = null) }
-                viewModelScope.launch(context = Dispatchers.IO) {
-                    val postToQuote = postDao.getMentionedPost(postId = nostrId.hex)
-                    preparePost(postToQuote = postToQuote, relays = nostrId.recommendedRelays)
-                }.invokeOnCompletion { isPreparing.set(false) }
-            }
-        }
+    val onPrepareQuote: (String) -> Unit = local@{ postIdToQuote ->
+        if (isPreparing.get() || uiState.value.postToQuote?.id == postIdToQuote) return@local
+        val nostrId = nostrStrToNostrId(postIdToQuote) ?: return@local
+
+        isPreparing.set(true)
+        viewModelState.update { it.copy(postToQuote = null) }
+        viewModelScope.launch(context = Dispatchers.IO) {
+            val postToQuote = getCleanMentionedPost(postIdHex = nostrId.hex)
+            preparePost(postToQuote = postToQuote, relays = nostrId.recommendedRelays)
+        }.invokeOnCompletion { isPreparing.set(false) }
     }
 
     private fun preparePost(postToQuote: MentionedPost?, relays: Collection<String> = emptyList()) {
         updateMetadataState()
-        Log.i(TAG, "Prepare new ${postToQuote?.let { "quoted " } ?: ""}post")
         viewModelState.update {
             it.copy(
                 pubkey = personalProfileProvider.getPubkey(),
@@ -99,52 +89,58 @@ class PostViewModel(
         }
     }
 
-    val onChangeContent: (String) -> Unit = { input ->
-        if (input != uiState.value.content) {
-            viewModelState.update {
-                it.copy(content = input, isSendable = input.isNotBlank() || it.postToQuote != null)
-            }
+    private suspend fun getCleanMentionedPost(postIdHex: String): MentionedPost? {
+        val mentionedPost = postDao.getMentionedPost(postId = postIdHex) ?: return null
+
+        if (mentionedPost.name.isNullOrEmpty()) {
+            val shortenedNpub = getShortenedNpubFromPubkey(mentionedPost.pubkey)
+            return mentionedPost.copy(name = shortenedNpub)
         }
+
+        return mentionedPost
+    }
+
+    val onChangeContent: (String) -> Unit = local@{ input ->
+        if (input == uiState.value.content) return@local
+        viewModelState.update {
+            it.copy(content = input, isSendable = input.isNotBlank() || it.postToQuote != null)
+        }
+
     }
 
     val onToggleRelaySelection: (Int) -> Unit = { index ->
         val toggled = toggleRelay(relays = uiState.value.relayStatuses, index = index)
         if (toggled.any { it.isActive }) {
-            viewModelState.update {
-                it.copy(relayStatuses = toggled)
-            }
+            viewModelState.update { it.copy(relayStatuses = toggled) }
         }
     }
 
     val onSend: () -> Unit = {
-        uiState.value.let { state ->
-            val selectedRelays = state.relayStatuses
-                .filter { it.isActive }
-                .map { it.relayUrl }
-            Log.i(TAG, "Send post to ${selectedRelays.size} relays")
-            val event = nostrService.sendPost(
-                content = getContentToPublish(state = state),
-                mentions = emptyList(), // TODO: Set mentions
-                hashtags = HashtagUtils.extractHashtagValues(state.content),
-                relays = selectedRelays
-            )
-            viewModelScope.launch(context = Dispatchers.IO) {
-                postDao.insertIfNotPresent(PostEntity.fromEvent(event))
-                // TODO: Insert hashtags in tx
-                // TODO: dbSweepExcludingCache.addPostId(event.id)
-                val hashtags = event.getHashtags()
-                    .map { HashtagEntity(eventId = event.id, hashtag = it) }
-                if (hashtags.isNotEmpty()) {
-                    hashtagDao.insertOrIgnore(*hashtags.toTypedArray())
-                }
+        val state = uiState.value
+        val selectedRelays = state.relayStatuses
+            .filter { it.isActive }
+            .map { it.relayUrl }
+        val event = nostrService.sendPost(
+            content = getContentToPublish(state = state),
+            mentions = emptyList(), // TODO: Set mentions
+            hashtags = HashtagUtils.extractHashtagValues(state.content),
+            relays = selectedRelays
+        )
+        viewModelScope.launch(context = Dispatchers.IO) {
+            postDao.insertIfNotPresent(PostEntity.fromEvent(event))
+            // TODO: Insert hashtags in tx
+            // TODO: dbSweepExcludingCache.addPostId(event.id)
+            val hashtags = event.getHashtags()
+                .map { HashtagEntity(eventId = event.id, hashtag = it) }
+            if (hashtags.isNotEmpty()) {
+                hashtagDao.insertOrIgnore(*hashtags.toTypedArray())
             }
-            showPostPublishedToast()
-            resetUI()
         }
+        showPostPublishedToast()
+        resetUI()
     }
 
     private fun resetUI() {
-        Log.i(TAG, "Reset Post view")
         viewModelState.update {
             it.copy(
                 content = "",
