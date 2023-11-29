bac8ee9af003046f9ff592cd045d3b11b1a4dde7
dluvian <dluvian@proton.me>
2023-11-29 12:44:50 +0800

    Add index on name and about

--- app/src/main/java/com/dluvian/nozzle/data/room/entity/ProfileEntity.kt
+++ app/src/main/java/com/dluvian/nozzle/data/room/entity/ProfileEntity.kt
@@ -6,7 +6,10 @@ import androidx.room.Index
 import androidx.room.PrimaryKey
 import com.dluvian.nozzle.model.nostr.Metadata
 
-@Entity(tableName = "profile", indices = [Index(value = ["pubkey"])])
+@Entity(
+    tableName = "profile",
+    indices = [Index(value = ["pubkey"], unique = true), Index(value = ["name", "about"])]
+)
 data class ProfileEntity(
     @PrimaryKey(autoGenerate = false) val pubkey: String,
     @Embedded val metadata: Metadata,
